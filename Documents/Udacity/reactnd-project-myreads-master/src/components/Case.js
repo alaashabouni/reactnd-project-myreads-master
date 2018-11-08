a229d340e94631ef862248b4e6161219f438adf3
Alaa Shabouni <alaashabouni@gmail.com>
2018-11-07 16:58:48 -0800

    Broke down App.js into components

---
+++ Documents/Udacity/reactnd-project-myreads-master/src/components/Case.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import Shelf from "./Shelf";
+
+class Case extends Component {
+  state = {
+
+  }
+
+
+    render() {
+        return (
+          <div className="app">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                  <Shelf/>
+              </div>
+              <div className="open-search">
+                <a onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              </div>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Case;
