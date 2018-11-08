f56a96ccdceb4385eff3aefcf957a3fe652bd7bf
Alaa Shabouni <alaashabouni@gmail.com>
2018-11-07 22:35:26 -0800

    Update Case.js part two

--- Documents/Udacity/reactnd-project-myreads-master/src/components/Case.js
+++ Documents/Udacity/reactnd-project-myreads-master/src/components/Case.js
@@ -2,11 +2,46 @@ import React, {Component} from 'react'
 import Shelf from "./Shelf";
 
 class Case extends Component {
-  state = {
+  state = {}
+
+  componentDidMount = () => {
+    this
+      .props
+      .onRefreshAllBooks();
   }
 
+  updateShelves = () => {
+    const newCurrentBook = {
+      name: "Currently Reading",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === 'currentlyReading')
+    };
 
+    const newWantedBook = {
+      name: "Want to Read",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === 'wantToRead')
+    };
+
+    const newReadBook = {
+      name: "Read",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === 'read')
+    };
+
+    return {[ newCurrentBook, newWantedBook, newReadBook]}
+  }
     render() {
+      let shelves = [];
+      if (this.props.books && this.props.books.length)
+        shelves = this.updateShelves();
+
         return (
           <div className="app">
           <div className="list-books">
@@ -15,7 +50,10 @@ class Case extends Component {
             </div>
             <div className="list-books-content">
               <div>
-                  <Shelf/>
+                  {shelves && shelves.map((shelf) => (<Shelf
+                    key={shelf.name}
+                    shelf={shelf}
+                    />))}
               </div>
               <div className="open-search">
                 <a onClick={() => this.setState({ showSearchPage: false })}>Close</a>
